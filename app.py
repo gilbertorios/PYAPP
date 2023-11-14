b1a8ecad1204e486f1773caf49023906776977df
gilbertorios <53060669+gilbertorios@users.noreply.github.com>
2023-11-14 19:39:48 +0000

    changes

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request 
+#import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+#setup app to use sqlalchemy database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sample.db'
+db = SQLAlchemy(app)
+
+
+#setup a simple table for database
+class Visitor(db.Model):
+    username = db.Column(db.String(100), primary_key = True)
+    numVisits = db.Column(db.Integer, default=1)
+
+    def __repr__(self):
+        return f"{self.username} - {self.numVisits}"
+    
+
+#create tables in database
+with app.app_context():
+    db.create_all()
+
+
 
 #Make a homepage
 @app.route('/')
@@ -15,4 +37,28 @@ def hello(name):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+@app.route('/form', methods=['GET', 'POST'])
+def formDemo():
+    if request.method == 'POST':
+        if request.form['name']:
+         name=request.form['name']
+         #check if user is in the database
+         visitor = Visitor.query.get(name)
+         if visitor == None:
+            #add visitor to the database
+            visitor = Visitor(username=name)
+            db.session.add(visitor)
+        else:
+            visitor.numVisits +=1
+
+    db.session.commit()
+    return render_template('form.html', name=name)
+
+@app.route('/visitors')
+def visitors():
+        #query the database to find all visitors
+        people = Visitor.query.all()
+        return render_template('visitors.html')
 
